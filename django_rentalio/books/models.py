60562239afed7082c036f9b49767f9a9cbd34e61
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-22 12:38:25 +0200

    QR_CODE GENERATION FUNCTIONALITY
    The functionality is dependent on the Qrcode and Pillow libraries; Inside books apps, models Book model addition of lines responsible for creating the qr_core inside the save function;
    the qr code will be created based on a book_id, afterwards it will be pasted to a canvas, the file name will be created based on a book's title and the canvas saved as a PNG image inside the buffer, then the
    buffered image data will be saved inside the qr_code field, finally it will be saved inside the database

--- django_rentalio/books/models.py
+++ django_rentalio/books/models.py
@@ -4,6 +4,10 @@ from django.db import models
 from publishers.models import Publisher
 from authors.models import Author
 from django.utils.text import slugify
+import qrcode
+from io import BytesIO
+from django.core.files import File
+from PIL import Image
 
 
 class BookTitle(models.Model):
@@ -38,6 +42,15 @@ class Book(models.Model):
 
     def save(self,*args,**kwargs):
         if not self.book_id:
-            self.book_id= str(uuid.uuid4()).replace('-','')[:30].lower()
-        super().save(*args,**kwargs)
+            self.book_id= str(uuid.uuid4()).replace('-','')[:24].lower()
 
+            qrcode_img=qrcode.make(self.book_id)                                                                        #QR_CODE FUNCTIONALITY
+            canvas = Image.new('RGB', (qrcode_img.pixel_size,qrcode_img.pixel_size), 'white')                           #qr code display canvas
+            canvas.paste(qrcode_img)
+            fname = f'qr_code-{self.title}.png'                                                                         #file name
+            buffer = BytesIO()
+            canvas.save(buffer,'PNG')
+            self.qr_code.save(fname,File(buffer),save=False)
+            canvas.close()
+
+        super().save(*args,**kwargs)
