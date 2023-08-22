552505563884465a1e15b5296e36271c81f5f2cb
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-22 11:51:00 +0200

    Creation of book models inside the books app
    1st model BookTitle has 2 ForeignKeys(author and publisher), it has a save function that will automaticate the creation of slugs and ensure that slugs are created
    for all the book titles; 2nd model Book has a single ForeignKey(BookTitle), and among other fields a qr_code variable ImageField that will set the rules of QR_codes generation, addition of a save function to the
    Book model that will assure smooth generation of book id only if it's not already been provided (it will be limited to 30 characters), running the migrations makemigrations/migrate

--- django_rentalio/books/models.py
+++ django_rentalio/books/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from publishers.models import Publisher
+from authors.models import Author
+from django.utils.text import slugify
+
+
+class BookTitle(models.Model):
+    #FK
+    publisher=models.ForeignKey(Publisher,on_delete=models.CASCADE)
+    author = models.ForeignKey(Author,on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=300,unique=True)
+    slug=models.SlugField(blank=True)                                                                                   #optional
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Book position:{self.title}"
+
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug=slugify(self.title)
+            super().save(*args,**kwargs)
+
+class Book(models.Model):
+    #FK
+    title = models.ForeignKey(BookTitle,on_delete=models.CASCADE)
+    book_id=models.CharField(max_length=50,blank=True)
+
+    qr_code=models.ImageField(upload_to='qr_codes',blank=True,null=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.title)
+
+    def save(self,*args,**kwargs):
+        if not self.book_id:
+            self.book_id= str(uuid.uuid4()).replace('-','')[:30].lower()
+        super().save(*args,**kwargs)
+
