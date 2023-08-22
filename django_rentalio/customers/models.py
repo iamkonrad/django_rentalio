afa73ffff6220608363e8804790ac49ae703e890
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-22 15:23:18 +0200

    Creation of Customer model inside the customers app
     Customer model having many to many relationship with books model, __str__(self) method used to define a string representation of the Customer username
    instance, custom save method will generate a unique username (based on concactenated first and last names) if there is none in the system, if a username checked already exists a numeric value will be appended
    to a username to make it unique, finally a unique username is assigned to a username field and saved into the db

--- django_rentalio/customers/models.py
+++ django_rentalio/customers/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from books.models import Book
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Customer(models.Model):
+    #Many To Many
+    books=models.ManyToManyField(Book,blank=True, help_text='books currently rented out')
+
+    first_name=models.CharField(max_length=300)
+    last_name=models.CharField(max_length=300)
+    username=models.CharField(max_length=300, blank=True, unique=True)
+    additional_info=models.TextField(blank=True)
+    rating=models.PositiveSmallIntegerField(default=50)
+    book_count=models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self):
+        return str(self.username)
+
+    def save(self, *args, **kwargs):
+        if not self.username:                                                                                           #if no username, generating one
+            username= slugify(f'{self.first_name} {self.last_name}')
+            ex = self.__class__.objects.filter(username=username).exists()
+
+            while ex:
+                i=len(self.__class__.objects.filter(first_name=self.first_name, last_name=self.last_name))
+                username = slugify(f'{self.first_name} {self.last_name}{i+1}')
+                ex = self.__class__.objects.filter(username=username).exists()
+
+            self.username=username
+        super().save(*args,**kwargs)
+
+
