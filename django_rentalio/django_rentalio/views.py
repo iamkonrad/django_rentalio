2b66421a639ccdb87e3ecdd2637a2ea3e0564a8d
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-24 16:25:06 +0200

    DARKMODE SWITCH FUNCTIONALITY
    Inside core app's url creation of a change function responsible for the logic behind the change "theme" functionality, first the function will check
    whether there is already an information regarding the session for a visiting user inside the session, in case there is upon clicking a toggle button the mode will be set to the opposite of the one stored, in
    case there is no such information the default will be set to dark mode, afterwards the HTTP Referer will redirect the user to the page he came from;  registering the change_theme functionality in urls.py;
    inside base.html adding an if statement referencing the dark mode, so the tailwind will correctly behave when our intention if to turn the darkmode on/off

--- django_rentalio/django_rentalio/views.py
+++ django_rentalio/django_rentalio/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from customers.models import Customer
-from books.models import Book, BookTitle
+from books.models import Book
 
+
+def change_theme(request):
+    if 'is_dark_mode' in request.session:
+        request.session['is_dark_mode'] = not request.session['is_dark_mode']
+    else:
+        request.session['is_dark_mode'] = True
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 def home_view(request):
     qs=Customer.objects.all()
     obj=Book.objects.get(id=1)
