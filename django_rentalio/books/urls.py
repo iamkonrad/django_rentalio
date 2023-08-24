a717b23323dc44dfb5d44a4d6dedcea1825eadb5
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-24 18:29:18 +0200

    Rewriting books app's book_title_list_view
    Rewriting books app's book_title_list_view function-based views to class-based view, adding a reference to class-based urls into the sidebar.html

--- django_rentalio/books/urls.py
+++ django_rentalio/books/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import book_title_list_view,book_title_detail_view
+from .views import BookTitleListView,book_title_detail_view
 
 
 app_name='books'
 
 urlpatterns = [
-    path('',book_title_list_view,name='main'),
-    path('<pk>/',book_title_detail_view,name='detail'),
+    path('', BookTitleListView.as_view(), name='main'),
+    path('<pk>/',book_title_detail_view,name='detail-book'),
 
 ]
 
--- django_rentalio/books/views.py
+++ django_rentalio/books/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from .models import BookTitle
+from django.views.generic import ListView
 
+#def book_title_list_view(request):
+#    qs=BookTitle.objects.all()
+#    return render(request, 'books/main.html',{'qs':qs})
 
-def book_title_list_view(request):
-    qs=BookTitle.objects.all()
-    return render(request, 'books/main.html',{'qs':qs})
+class BookTitleListView(ListView):
+    queryset=BookTitle.objects.all()
+    model = BookTitle
+    template_name='books/main.html'
+
+    def get_queryset(self):
+        parameter="a"
+        return BookTitle.objects.filter(title__startswith=parameter)
 
 def book_title_detail_view(request,**kwargs):
     pk=kwargs.get('pk')
