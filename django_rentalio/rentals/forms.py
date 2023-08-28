1fbdf860eed62b95c6418aee964b8464f2299d06
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-28 16:11:33 +0200

    SEARCH BY THE BOOK ID 1.
    Inside rentals app's views creation of search_book_view function, form variable initializes the form intance previously created in rentals/forms, search_query retrieves the
    value of search from the searchbar, while book_ex checks whether there is a particular book with the ISB equal to search query request

---
+++ django_rentalio/rentals/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+
+
+class SearchBookForm(forms.Form):
+    search = forms.CharField(label='',widget=forms.TextInput(attrs={'placeholder':'search by book id...'}))
+
--- django_rentalio/rentals/views.py
+++ django_rentalio/rentals/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .forms import SearchBookForm
+from books.models import Book
 
 def search_book_view(request):
-    context = {}
+    form=SearchBookForm(request.POST or None)
+    search_query = request.POST.get('search', None)
+    book_ex = Book.objects.filter(ISBN=search_query).exists()
+
+    if search_query is not None and book_ex:
+        pass
+    context = {
+        'form':form,
+    }
     return render(request,'rentals/main.html', context)
