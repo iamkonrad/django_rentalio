9bedf4d9b88088438f7a0fb58051a334a84aeda4
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-08-28 19:24:45 +0200

    SEARCH BY THE BOOK ID 3. REDIRECT TO RENTALS
    Inside rentals app creation of a BookRentalHistoryView class based view taking in rental model and setting the detail.html template for use; adding a url reference to
    BookRentalHistoryView, adding a url reference to the BookRentalHistory; creating and styling detail.html template; adding a reference to rentals/main.html inside a search_book_view, now it's possible to find a book by
    the book id

--- django_rentalio/rentals/urls.py
+++ django_rentalio/rentals/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from.views import search_book_view
+from.views import search_book_view, BookRentalHistoryView
 
 
 app_name = 'rentals'
 
 urlpatterns = [
     path('', search_book_view, name='main'),
+    path('<str:book_id>/',BookRentalHistoryView.as_view(),name="detail"),
+
 ]
--- django_rentalio/rentals/views.py
+++ django_rentalio/rentals/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import SearchBookForm
 from books.models import Book
+from django.views.generic import ListView
+from. models import Rental
 
 def search_book_view(request):
     form=SearchBookForm(request.POST or None)
@@ -8,8 +10,12 @@ def search_book_view(request):
     book_ex = Book.objects.filter(ISBN=search_query).exists()
 
     if search_query is not None and book_ex:
-        pass
+        return redirect ('rentals:detail',search_query)
     context = {
         'form':form,
     }
     return render(request,'rentals/main.html', context)
+
+class BookRentalHistoryView(ListView):
+    model = Rental
+    template_name='rentals/detail.html'
