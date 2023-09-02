4241c392a0f9382f2a4a80aa17de590780a8e072
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-09-02 22:30:30 +0200

    DASHBOARD: PART 3 CHART DATA a
    Inside main app's views creating an empty data list and all_books and all_book_titles variable that will count the number of book objects and book_titles; creating a dictionary
    and appending it to data list, grabbing the titles by publisher and creation of publisher_names list grabbing the publisher_names list using list comprehension, also grabbing the publisher_name_count and referencing
    it using a list comprehension, appending title_by_publisher, publisher_names and publisher_name_count to the original data list

--- django_rentalio/django_rentalio/views.py
+++ django_rentalio/django_rentalio/views.py
@@ -9,7 +9,25 @@ class DashboardView(TemplateView):
     template_name='dashboard.html'
 
 def chart_data(request):
-    qs = Book.objects.values('title__title').annotate(Count('title'))
+    data = []
+    all_books = len(Book.objects.all())
+    all_book_titles = len(BookTitle.objects.all())
+    data.append({
+        'labels':['books','book titles'],
+        'data':[all_books,all_book_titles],
+        'description':'unique book titles vs books',
+        'type':'bar',
+    })
+
+    titles_by_publisher = BookTitle.objects.values('publisher__name').annotate(Count('publisher__name'))
+    publisher_names=[x['publisher__name'] for x in titles_by_publisher]
+    publisher_names_count=[x['publisher__name__count'] for x in titles_by_publisher]
+    data.append({
+        'labels':publisher_names,
+        'data':publisher_names_count,
+        'description':'book title count by publisher',
+        'type':'pie',
+    })
     return JsonResponse
 def change_theme(request):
     if 'is_dark_mode' in request.session:
