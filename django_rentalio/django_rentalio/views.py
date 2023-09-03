63580f2f821aa4ee180a9a223ba66d4e76e62185
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-09-03 14:40:31 +0200

    DASHBOARD: PART 4 CHART DATA b
    Inside main app's views DashboardView class view creation of a book by status variable annotating and counting all the book titles based on their rental status, addition of a book_title_count variable
    using a list comprehension over each dictionary in the book_by_status list getting the value related to the book__title__count, creation of a status_keys variable and performing list comprehension over dictionaries in
    the book_by_status list and extracting the values associated with the status key and adding it to the status list, creation of a status variable transforming status choices list into a dictionary and afterwards
    performing a list comprehension over the keys in status keys returning the results in a readable form and storing them as a list inside the status variable, appending the books by status related data to the original data list;
    grabbing all the records associated with Customers and Publishers models from the db and storing their numbers in the customers and publishers variables using the len method and appending the data to the original data list

--- django_rentalio/django_rentalio/views.py
+++ django_rentalio/django_rentalio/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponseRedirect, JsonResponse
 from customers.models import Customer
 from books.models import Book, BookTitle
 from django.views.generic import TemplateView
-from django.db.models import Count, Sum
+from django.db.models import Count
+from rentals.models import Rental
+from publishers.models import Publisher
+from rentals.choices import STATUS_CHOICES
 
 class DashboardView(TemplateView):
     template_name='dashboard.html'
@@ -28,6 +30,27 @@ def chart_data(request):
         'description':'book title count by publisher',
         'type':'pie',
     })
+
+    book_by_status = Rental.objects.values('status').annotate(Count('book__title'))
+    book_title_count = [x['book__title__count'] for x in book_by_status]
+    status_keys = [x['status'] for x in book_by_status]
+    status = [dict(STATUS_CHOICES)[key] for key in status_keys]
+    data.append({
+        'labels':status,
+        'data':book_title_count,
+        'description':'book by status',
+        'type':'pie'
+    })
+
+    customers = len(Customer.objects.all())
+    publishers = len(Publisher.objects.all())
+    data.append({
+        'labels':['customers','publishers'],
+        'data':[customers,publishers],
+        'description': 'customers vs publishers',
+        'type':'bar'
+    })
+
     return JsonResponse
 def change_theme(request):
     if 'is_dark_mode' in request.session:
