aeea8c07548e579d3d370af38fec58ae7277312d
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-09-08 23:31:08 +0200

    Adding searching by authors
    Inside authors app working on finding a book by author's name

--- django_rentalio/authors/models.py
+++ django_rentalio/authors/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from django.urls import reverse
+from django.utils.text import slugify
+
 
 
 class Author(models.Model):
-    name=models.CharField(max_length=300)
+    name = models.CharField(max_length=300)
+    slug = models.SlugField(max_length=300, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return str(self.name)
+
+    def get_absolute_url(self):
+        return reverse('authors:details', args=[str(self.slug)])
+
--- django_rentalio/authors/views.py
+++ django_rentalio/authors/views.py
@@ -1,17 +1,36 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import FormView, ListView
-
+import string
+from django.views.generic import FormView, ListView, DetailView
+from books.models import BookTitle
 from authors.models import Author
 
 
 # Create your views here.
 
 
-
 class AuthorsListView(LoginRequiredMixin, ListView):
     model = Author
     template_name = 'authors/main.html'
 
+    def get_queryset(self):
+        parameter = self.kwargs.get('letter') or 'A'
+        if parameter.isdigit():
+            return Author.objects.filter(name__startswith=parameter)
+        else:
+            return Author.objects.filter(name__istartswith=parameter)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        letters = list(string.ascii_uppercase)
+        numbers = [str(i) for i in range(10)]
+
+        context['letters'] = letters
+        context['numbers'] = numbers
+        context['selected_letter'] = self.kwargs.get('letter') or 'A'
+        return context
 
 
+class AuthorDetailView(LoginRequiredMixin,DetailView):
+    model = BookTitle
+    template_name = 'authors/details.html'
