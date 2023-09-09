481ad08038b068f981245c276dd3fa33958dab69
iamkonrad <133384502+iamkonrad@users.noreply.github.com>
2023-09-09 21:54:30 +0200

    Searching for authors based on their last name
    Adding a formatted_name method to author's app authors model, which first splits the name into 2 parts based on the space between them and then puts the last element in the front and joins the original first element into it,
     so that search by author can return the authors names in format: last name + first name; adding the reference to formatted_name method into main.html, now when searching for authors, their names will be displayed in format last
    name + first  name, decided to use this method instead of changing the name variable, since it was only needed to display names in such format only for the purpose of using it in one template

--- django_rentalio/django_rentalio/authors/models.py
+++ django_rentalio/django_rentalio/authors/models.py
@@ -14,3 +14,9 @@ class Author(models.Model):
     def get_absolute_url(self):
         return reverse('authors:details', args=[self.name])
 
+    @property
+    def formatted_name(self):
+        name_parts = self.name.split()
+        if len(name_parts) >= 2:
+            return f"{name_parts[-1]}, {' '.join(name_parts[:-1])}"
+        return self.name
